561b9d3737aa7d0eab1d848ca211b3fcf6064cee
hugo.talbot <hugo.talbot@tpi17.cpe.fr>
2016-11-07 17:17:01 +0100

    v2.1

--- TrivialPursuit/src/Database.java
+++ TrivialPursuit/src/Database.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import org.apache.derby.jdbc.EmbeddedDriver; //trytryr
+import org.apache.derby.jdbc.EmbeddedDriver;
 
 public class Database {
 
