6006f9f885186a40c32061e98afa206d36efd2f9
hugo.talbot <hugo.talbot@tpi17.cpe.fr>
2016-11-07 17:38:51 +0100

    v2.2

--- TrivialPursuit/src/Database.java
+++ TrivialPursuit/src/Database.java
@@ -20,35 +20,44 @@ public class Database {
       PreparedStatement pstmt;
       Statement stmt;
       ResultSet rs = null;
-      String createSQL = "create table person ("
+      
+      String createSQL = "create table questions ("
       + "id integer not null generated always as"
       + " identity (start with 1, increment by 1),   "
-      + "name varchar(30) not null, "
-      + "email varchar(30),phone varchar(10),"
+      + "question varchar(100) not null, "
+      + "answer1 varchar(100),"
+      + "answer2 varchar(100),"
+      + "answer3 varchar(100),"
+      + "category varchar(100),"
+      + "correctanswer integer,"
       + "constraint primary_key primary key (id))";
-
+ 
       try {
          Driver derbyEmbeddedDriver = new EmbeddedDriver();
          DriverManager.registerDriver(derbyEmbeddedDriver);
          conn = DriverManager.getConnection("jdbc:derby:database;create=true");
          conn.setAutoCommit(false);
          stmt = conn.createStatement();
+         stmt.execute("drop table questions");
          stmt.execute(createSQL);
 
-         pstmt = conn.prepareStatement("insert into person (name,email,phone) values(?,?,?)");
-         pstmt.setString(1, "Hagar the Horrible");
-         pstmt.setString(2, "hagar@somewhere.com");
-         pstmt.setString(3, "1234567890");
+         pstmt = conn.prepareStatement("insert into questions (question,answer1,answer2,answer3,category,correctanswer) values(?,?,?,?,?,?)");
+         pstmt.setString(1, "De quel couleur était le cheval blanc d'Henry IV ?");
+         pstmt.setString(2, "Noir");
+         pstmt.setString(3, "Blanc");
+         pstmt.setString(4, "Gris");
+         pstmt.setString(5, "Histoire");
+         pstmt.setInt(6, 2);
          pstmt.executeUpdate();
 
-         rs = stmt.executeQuery("select * from person");
+         rs = stmt.executeQuery("select * from questions");
          while (rs.next()) {
-            System.out.printf("%d %s %s %s\n",
+            System.out.printf("%d %s %s %s %s %s %d\n",
             rs.getInt(1), rs.getString(2),
-            rs.getString(3), rs.getString(4));
+            rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getInt(7));
          }
 
-         stmt.execute("drop table person");
+         //stmt.execute("drop table questions");
 
          conn.commit();
 
