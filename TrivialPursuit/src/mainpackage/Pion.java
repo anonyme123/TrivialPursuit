1223c61160afb152c4a506d5c9aa219e10229deb
hugo.talbot <hugo.talbot@tpi20.cpe.fr>
2016-11-09 13:35:30 +0100

    renommage de Trivialursuit.java en IHM.java

---
+++ TrivialPursuit/src/mainpackage/Pion.java
@@ -0,0 +1,44 @@
+package mainpackage;
+
+public class Pion {
+
+	public String couleur;
+	public float positionDepartX;
+	public float positionDepartY;
+	public float positionArriveX;
+	public float positionArriveY;
+	public int idPion;
+	
+	public Pion(String couleur, float positionDepartX, float positionDepartY, float positionArriveX,
+			float positionArriveY, int idPion){
+		
+		this.couleur = couleur;
+		this.positionDepartX = positionDepartX;
+		this.positionDepartY = positionDepartY;
+		this.positionArriveX = positionArriveX;
+		this.positionArriveY = positionArriveY;
+		this.idPion = idPion;
+		
+	}
+	
+	public Pion(String couleur, float positionDepartX, float positionDepartY, int idPion){
+		this.couleur = couleur;
+		this.positionDepartX = positionDepartX;
+		this.positionDepartY = positionDepartY;
+		this.idPion = idPion;
+	}
+
+	@Override
+	public String toString() {
+		return " Le Pion est de couleur" + couleur +  " a pour coordonn�es de en X et Y de : " + positionDepartX +" " + 
+				positionDepartY + ", et se d�place en :" + positionArriveX + " "+ positionArriveY
+				+ ". Il s'agit du pion num�ro " + idPion;
+	}
+	
+	public String toString1() {
+		return " Le Pion est de couleur" + couleur +  " a pour coordonn�es de en X et Y de : " + positionDepartX +" " + 
+				positionDepartY + ". Il s'agit du pion num�ro " + idPion;
+	}
+	
+	
+}
