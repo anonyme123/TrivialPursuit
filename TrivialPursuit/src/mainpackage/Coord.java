9d0334851a77eaf0b11437e8de1b95d5cf5a841c
Sébastien <Sbastien@10.170.0.213>
2016-11-10 09:59:45 +0100

    peti commit OKLM

--- TrivialPursuit/src/mainpackage/Coord.java
+++ TrivialPursuit/src/mainpackage/Coord.java
@@ -11,35 +11,19 @@ public class Coord implements Serializable {
 	
 	public int x, y;
 	
-	/**
-	 * @param x
-	 * @param y
-	 */
+	//Constructeur des coordonnées
 	public Coord(int x, int y) {
 		this.x = x; 
 		this.y = y;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
+	
 	@Override
 	public String toString() {
 		return "[x=" + x + ", y=" + y + "]";
 	}
 	
-	/*
-	 * @param x
-	 * @param y
-	 * @return true si les coordonnï¿½es sont valides (dans un plateau de 8*8)
-	 */
-/*	public static boolean coordonnees_valides(int x, int y){    // A CHANGER !!!!! 
-		return ( (x<=7) && (x>=0) && (y<=7) && (y>=0) );
-	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -49,9 +33,7 @@ public class Coord implements Serializable {
 		return result;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
