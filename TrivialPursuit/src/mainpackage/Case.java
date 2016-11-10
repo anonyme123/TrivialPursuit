c4681be9dcb28a12349b9c608f28c9ae225da19a
gael.venin <gael.venin>
2016-11-10 08:31:01 +0100

    classe case

---
+++ TrivialPursuit/src/mainpackage/Case.java
@@ -0,0 +1,23 @@
+//Classe CASE
+//Date 10/11/2016
+//-------------------------
+
+
+package mainpackage;
+
+public class Case {
+
+	
+	//Attributs
+	public String categorie;
+	public int largeur;
+	public int longueur;
+	public int posX;
+	public int posY;
+	public boolean camembert;
+	//-------------------------
+	
+	
+	
+	
+}
