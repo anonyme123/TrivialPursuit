05027b27bb426a0e6ed24a0a13b2f1b7cbc6af5a
gael.venin <gael.venin>
2016-11-25 12:02:02 +0100

    MAJ 12:00

--- trivia/src/model/CaseBonus.java
+++ trivia/src/model/CaseBonus.java
@@ -1,15 +1,15 @@
+//AUTEUR : Gael Venin
+
+//DESCRIPTION: 
+//Classe CaseBonus, herite de CaseCam. Permet de creer la case unique BONUS au centre du jeu
+
 package model;
 
-	
 
 public class CaseBonus extends CaseCam {
-
+	
 	public CaseBonus(Couleur couleur, int posX, int posY) {
 		super(couleur, posX, posY);
 		// TODO Auto-generated constructor stub		
-		
-	}
-
-	
-	
+	}	
 }
